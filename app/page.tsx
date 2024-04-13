96760fcba26611426d897e38bcc33b54b69ba87d
dekmaster <34228186+dekmaster@users.noreply.github.com>
2024-04-13 12:04:02 -0700

    try to get facebook approval

--- app/page.tsx
+++ app/page.tsx
@@ -31,11 +31,11 @@ export default function Example() {
           <div className="mt-24 sm:mt-32 lg:mt-16">
             <a href="#" className="inline-flex space-x-6">
               <span className="rounded-full bg-indigo-600/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-600 ring-1 ring-inset ring-indigo-600/10">
-                What&apos;s new
+                WebEasy.AI
               </span>
               <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-600">
-                <span>In Development</span>
-                <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                <span>Coming Soon</span>
+                
               </span>
             </a>
           </div>
