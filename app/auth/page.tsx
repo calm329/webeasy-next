dea5b37433ebe66a2d66ea52f5d48e6e63ef9e94
calm <calmdev0329@gmail.com>
2024-04-18 22:43:30 +0800

    feat: optimize using nextjs' features, split components from pages

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -8,6 +8,13 @@ import {
 import unirest from "unirest";
 import OpenAI from "openai";
 import { createNewUser } from "@/lib/actions";
+import Image from "next/image";
+import { ServiceIcon } from "@/components/icons";
+import Link from "next/link";
+import PostCard from "@/components/card/post-card";
+import ServiceCard from "@/components/card/service-card";
+import CTA from "@/components/cta";
+import TopBar from "@/components/top-bar";
 
 async function getMedia(accessToken: string) {
   //get user media
@@ -311,37 +318,14 @@ export default async function Page({
     <div>
       <section className="bg-white py-6">
         <div className="container mx-auto px-4">
-          <div className="flex items-center justify-between rounded-full border border-gray-100 bg-gray-100 px-6 py-3.5">
-            <div className="w-auto">
-              <div className="flex flex-wrap items-center">
-                <div
-                  className="text-black-300 w-auto text-xl font-medium"
-                  style={{ color: aiObj["colors"]["primary"] }}
-                >
-                  <a href="#">{aiObj["businessName"]}</a>
-                </div>
-              </div>
-            </div>
-            <div className="w-auto">
-              <div className="flex flex-wrap items-center">
-                <div className="w-auto lg:block">
-                  <div className="-m-2 flex flex-wrap">
-                    <div className="w-full p-2 md:w-auto">
-                      <a
-                        className="block w-full rounded-full bg-blue-500 px-4 py-2.5 text-center text-sm font-bold text-white hover:bg-blue-600 focus:ring-4 focus:ring-blue-200"
-                        style={{
-                          backgroundColor: aiObj["colors"]["secondary"],
-                        }}
-                        href="#"
-                      >
-                        {aiObj["hero"]["cta"]}
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <TopBar
+            businessName={aiObj["businessName"]}
+            colors={aiObj["colors"]}
+            cta={{
+              text: aiObj["hero"]["cta"],
+              link: "#",
+            }}
+          />
         </div>
       </section>
       <section className="overflow-hidden bg-gray-50 py-10">
@@ -362,68 +346,34 @@ export default async function Page({
                     </p>
                     <div className="-m-2 flex flex-wrap">
                       <div className="w-full p-2 md:w-auto">
-                        <a
-                          className="block w-full rounded-full bg-blue-500 px-8 py-3.5 text-center text-lg font-bold text-white hover:bg-blue-600 focus:ring-4 focus:ring-blue-200"
-                          style={{
-                            backgroundColor: aiObj["colors"]["secondary"],
-                          }}
-                          href="#"
-                        >
-                          {aiObj["hero"]["cta"]}
-                        </a>
+                        <CTA
+                          text={aiObj["hero"]["cta"]}
+                          bgColor={aiObj["colors"]["secondary"]}
+                          link="#"
+                        />
                       </div>
                     </div>
                   </div>
                 </div>
                 <div className="w-full p-8 md:w-1/2">
-                  <img
-                    className="mx-auto rounded-3xl object-contain md:mr-0"
-                    width="256"
-                    height="256"
+                  <Image
                     src={aiObj["hero"]["imageUrl"]}
-                    alt=""
+                    width={256}
+                    height={256}
+                    alt="Hero Image"
+                    className="mx-auto rounded-3xl object-contain md:mr-0"
                   />
                 </div>
               </div>
               <div className="rounded-3xl bg-gray-100 p-8 md:p-12">
                 <div className="-m-8 flex flex-wrap">
                   {aiObj["services"]["list"].map((service) => (
-                    <div key={0} className="w-full p-8 md:w-1/3">
-                      <div className="-m-3 flex flex-wrap">
-                        <div className="w-auto p-3 md:w-full lg:w-auto">
-                          <div className="flex h-12 w-12 items-center justify-center rounded-xl bg-white">
-                            <svg
-                              width={24}
-                              height={24}
-                              viewBox="0 0 24 24"
-                              fill="none"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                d="M14 16C14 17.77 13.23 19.37 12 20.46C10.94 21.42 9.54 22 8 22C4.69 22 2 19.31 2 16C2 13.9753 3.01397 12.1814 4.5554 11.0973C4.80358 10.9228 5.1393 11.0422 5.27324 11.3145C6.21715 13.2332 7.95419 14.6699 10.02 15.23C10.65 15.41 11.31 15.5 12 15.5C12.4872 15.5 12.9539 15.4538 13.4074 15.3687C13.6958 15.3147 13.9828 15.4995 13.9955 15.7926C13.9985 15.8621 14 15.9314 14 16Z"
-                                fill={aiObj["colors"]["primary"]}
-                              />
-                              <path
-                                d="M18 8C18 8.78 17.85 9.53 17.58 10.21C16.89 11.95 15.41 13.29 13.58 13.79C13.08 13.93 12.55 14 12 14C11.45 14 10.92 13.93 10.42 13.79C8.59 13.29 7.11 11.95 6.42 10.21C6.15 9.53 6 8.78 6 8C6 4.69 8.69 2 12 2C15.31 2 18 4.69 18 8Z"
-                                fill={aiObj["colors"]["primary"]}
-                              />
-                              <path
-                                d="M22 16C22 19.31 19.31 22 16 22C15.2555 22 14.5393 21.8643 13.8811 21.6141C13.5624 21.4929 13.503 21.0851 13.7248 20.8262C14.8668 19.4938 15.5 17.786 15.5 16C15.5 15.66 15.47 15.32 15.42 15C15.3902 14.8155 15.4844 14.6342 15.6478 14.5437C16.9719 13.8107 18.0532 12.6876 18.727 11.3153C18.8609 11.0427 19.1968 10.923 19.4452 11.0978C20.9863 12.1818 22 13.9755 22 16Z"
-                                fill={aiObj["colors"]["primary"]}
-                              />
-                            </svg>
-                          </div>
-                        </div>
-                        <div className="flex-1 p-3">
-                          <h3 className="font-heading mb-2 text-xl font-black text-gray-900">
-                            {service["name"]}
-                          </h3>
-                          <p className="text-sm font-bold text-gray-700">
-                            {service["description"]}
-                          </p>
-                        </div>
-                      </div>
-                    </div>
+                    <ServiceCard
+                      key={service["name"]}
+                      name={service["name"]}
+                      description={service["description"]}
+                      color={aiObj["colors"]["primary"]}
+                    />
                   ))}
                 </div>
               </div>
@@ -439,30 +389,15 @@ export default async function Page({
 
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {iPosts.map((post) => (
-            <a key={post.id} className="group">
-              <div className="aspect-h-1 aspect-w-1 xl:aspect-h-8 xl:aspect-w-7 w-full overflow-hidden rounded-lg bg-gray-200">
-                {post.media_type === "VIDEO" ? (
-                  <video
-                    src={post.media_url}
-                    className="h-full w-full object-cover object-center group-hover:opacity-75"
-                    autoPlay
-                    loop
-                    muted
-                    playsInline
-                  />
-                ) : (
-                  <img
-                    src={post.media_url}
-                    alt={post.caption}
-                    className="h-full w-full object-cover object-center group-hover:opacity-75"
-                  />
-                )}
-              </div>
-              <h3 className="mt-4 text-sm text-gray-700">{post.caption}</h3>
-              <p className="mt-1 text-xs font-medium text-gray-900">
-                {post.timestamp}
-              </p>
-            </a>
+            <PostCard
+              key={post.id}
+              id={post.id}
+              permalink={post.permalink}
+              media_url={post.media_url}
+              media_type={post.media_type}
+              caption={post.caption}
+              timestamp={post.timestamp}
+            />
           ))}
         </div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,25 @@
+import { GridBoxIcon } from "@/components/icons";
 import { ChevronRightIcon } from "@heroicons/react/20/solid";
+import Image from "next/image";
+import tailwindIcon from "@/public/tailwind.svg";
+import Link from "next/link";
 
 export default function Example() {
   return (
     <div className="relative isolate overflow-hidden bg-white">
-      <svg
-        className="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
-        aria-hidden="true"
-      >
-        <defs>
-          <pattern
-            id="0787a7c5-978c-4f66-83c7-11c213f99cb7"
-            width={200}
-            height={200}
-            x="50%"
-            y={-1}
-            patternUnits="userSpaceOnUse"
-          >
-            <path d="M.5 200V.5H200" fill="none" />
-          </pattern>
-        </defs>
-        <rect
-          width="100%"
-          height="100%"
-          strokeWidth={0}
-          fill="url(#0787a7c5-978c-4f66-83c7-11c213f99cb7)"
-        />
-      </svg>
+      <GridBoxIcon className="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]" />
       <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
         <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl lg:flex-shrink-0 lg:pt-8">
-          <img
-            className="h-11"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
+          <Image src={tailwindIcon} alt="Your Company" className="h-11" />
           <div className="mt-24 sm:mt-32 lg:mt-16">
-            <a href="#" className="inline-flex space-x-6">
+            <Link href="#" className="inline-flex space-x-6">
               <span className="rounded-full bg-indigo-600/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-600 ring-1 ring-inset ring-indigo-600/10">
                 WebEasy.AI
               </span>
               <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-600">
                 <span>Coming Soon</span>
               </span>
-            </a>
+            </Link>
           </div>
           <h1 className="mt-10 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
             Instantly create your website from your Instagram profile
@@ -50,18 +28,18 @@ export default function Example() {
             Connect you instagram profile and we will create a website for you.
           </p>
           <div className="mt-10 flex items-center gap-x-6">
-            <a
+            <Link
               href={`${process.env.INSTAGRAM_API_AUTH_ENDPOINT}authorize?client_id=${process.env.NEXT_PUBLIC_FB_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_FB_REDIRECT_URL}&scope=user_profile,user_media&response_type=code`}
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Connect Instagram
-            </a>
-            <a
+            </Link>
+            <Link
               href="#"
               className="text-sm font-semibold leading-6 text-gray-900"
             >
               Learn more <span aria-hidden="true">→</span>
-            </a>
+            </Link>
           </div>
         </div>
         <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
@@ -73,7 +51,7 @@ export default function Example() {
                 width={2432}
                 height={1442}
                 className="w-[76rem] rounded-md shadow-2xl ring-1 ring-gray-900/10"
-  />*/}
+              />*/}
             </div>
           </div>
         </div>
