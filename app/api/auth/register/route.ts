b1b1f461f541c8edebabeb06f72d55c232b2c442
calm <calmdev0329@gmail.com>
2024-04-26 01:33:34 +0800

    fix: type error in register api

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,6 +1,8 @@
 import bcrypt from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+import { unknown } from "zod";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 export async function POST(req: NextRequest) {
   const { name, email, password } = await req.json();
@@ -23,13 +25,17 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json(user);
   } catch (error) {
-    if (error.code === "P2002") {
+    const unknownError = error as any;
+    if (unknownError.code === "P2002") {
       return NextResponse.json(
         { error: "Email already exists" },
         { status: 400 },
       );
     } else {
-      return NextResponse.json({ error: error.message }, { status: 500 });
+      return NextResponse.json(
+        { error: unknownError.message },
+        { status: 500 },
+      );
     }
   }
 }
