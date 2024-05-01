e51818fee6d3a2a51462783bb7df49b58834f507
calm <calmdev0329@gmail.com>
2024-05-01 15:10:23 +0200

    build issue fixed

--- components/templates/basic-template.tsx
+++ components/templates/basic-template.tsx
@@ -23,9 +23,9 @@ type BasicTemplateProps = {
   };
   services: any[];
   posts: any[];
-  open:boolean,
-  setIsOpen:Dispatch<SetStateAction<boolean>>
-  setSection:Dispatch<SetStateAction<number>>
+  open?:boolean,
+  setIsOpen?:Dispatch<SetStateAction<boolean>>
+  setSection?:Dispatch<SetStateAction<number>>
 };
 
 export default function BasicTemplate(props: BasicTemplateProps) {
@@ -45,7 +45,7 @@ export default function BasicTemplate(props: BasicTemplateProps) {
   return (
     <>
       <section className="bg-white py-6">
-        <div className="container mx-auto px-4 hover:border hover:border-red-500" onClick={()=>{setIsOpen(true);setSection(1)}}>
+        <div className="container mx-auto px-4 hover:border hover:border-red-500" onClick={()=>{if(setIsOpen && setSection){setIsOpen(true);setSection(1)}}}>
           <TopBar
             logo={logo || ""}
             businessName={businessName}
@@ -58,7 +58,7 @@ export default function BasicTemplate(props: BasicTemplateProps) {
         <div className="container mx-auto px-4">
           <div className="rounded-3xl bg-white px-8 py-16">
             <div className="mx-auto max-w-7xl">
-              <div className="-m-8 mb-10 flex flex-wrap hover:border hover:border-blue-500" onClick={()=>{setIsOpen(true); setSection(2)}}>
+              <div className="-m-8 mb-10 flex flex-wrap hover:border hover:border-blue-500" onClick={()=>{if(setIsOpen && setSection){setIsOpen(true); setSection(2)}}}>
                 <div className="w-full p-8 md:w-1/2">
                   <div className="md:max-w-lg">
                     <h2
