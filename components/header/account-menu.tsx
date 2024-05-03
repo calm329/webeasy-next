70b3e019cac49656ef8897abba5e6b00a0cfd54d
shubhamrakhecha5@gmail.com <shubhamrakhecha5@gmail.com>
2024-05-04 01:04:16 +0530

    added account-menu to /settings

--- components/header/account-menu.tsx
+++ components/header/account-menu.tsx
@@ -46,7 +46,7 @@ export default function AccountMenu() {
             <Menu.Item>
               {({ active }) => (
                 <Link
-                  href="#"
+                  href="/settings"
                   className={classNames(
                     active ? "bg-gray-100 text-gray-900" : "text-gray-700",
                     "block px-4 py-2 text-sm",
