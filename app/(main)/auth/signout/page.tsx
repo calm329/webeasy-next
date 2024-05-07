308ebaac14f684d835b16b1f79651d2a5472e5af
calm <calmdev0329@gmail.com>
2024-05-07 23:43:13 +0530

    update in signout

--- app/(main)/auth/signout/page.tsx
+++ app/(main)/auth/signout/page.tsx
@@ -4,7 +4,13 @@ import React, { useEffect } from "react";
 
 const SignOut = () => {
   useEffect(() => {
-    signOut({ callbackUrl: "/" });
+    signOut({ callbackUrl: "/" })
+      .then(() => {
+        console.log("successfully signed out");
+      })
+      .catch(() => {
+        console.log("failed to sign out");
+      });
   }, []);
   return null;
 };
