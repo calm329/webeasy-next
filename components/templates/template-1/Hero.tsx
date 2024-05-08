89d4d2b46ae9f8cc8be3a3ecdb6921252978c3a7
shubhamrakhecha5@gmail.com <shubhamrakhecha5@gmail.com>
2024-05-08 23:59:00 +0530

    updated design for template-1

--- components/templates/template-1/Hero.tsx
+++ components/templates/template-1/Hero.tsx
@@ -5,16 +5,16 @@ import Image from "next/image";
 export function Hero() {
   return (
     <div className="relative py-20 sm:pb-24 sm:pt-36 ">
-      <BackgroundImage className="-bottom-14 -top-36" />
-      <Container className="relative flex">
+      <BackgroundImage className="-bottom-14 -top-36 " />
+      <Container className="relative flex max-lg:flex-col-reverse">
         <Image
           src={"/images/bird.jpg"}
           alt=""
           height={400}
           width={300}
-          className="rounded-lg object-contain drop-shadow"
+          className="mt-5 rounded-lg object-contain drop-shadow max-lg:mx-auto"
         />
-        <div className="ml-auto max-w-2xl lg:max-w-4xl lg:px-12">
+        <div className="ml-auto max-w-2xl max-lg:mx-auto lg:max-w-4xl lg:px-12">
           <h1 className="font-display text-5xl font-bold tracking-tighter text-blue-600 sm:text-7xl">
             <span className="sr-only">DeceptiConf - </span>A design conference
             for the dark side.
@@ -32,24 +32,9 @@ export function Hero() {
               how to deploy them without ever being detected.
             </p>
           </div>
-          <Button href="#" className="mt-10 w-full sm:hidden">
+          <Button href="#" className="mt-10 w-full ">
             Get your tickets
           </Button>
-          <dl className="mt-10 grid grid-cols-2 gap-x-10 gap-y-6 sm:mt-16 sm:gap-x-16 sm:gap-y-10 sm:text-center lg:auto-cols-auto lg:grid-flow-col lg:grid-cols-none lg:justify-start lg:text-left">
-            {[
-              ["Speakers", "18"],
-              ["People Attending", "2,091"],
-              ["Venue", "Staples Center"],
-              ["Location", "Los Angeles"],
-            ].map(([name, value]) => (
-              <div key={name}>
-                <dt className="font-mono text-sm text-blue-600">{name}</dt>
-                <dd className="mt-0.5 text-2xl font-semibold tracking-tight text-blue-900">
-                  {value}
-                </dd>
-              </div>
-            ))}
-          </dl>
         </div>
       </Container>
     </div>
