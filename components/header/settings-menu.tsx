f1db7df280eb6b49bc2229772871ea52ccdd4b46
shubhamrakhecha5@gmail.com <shubhamrakhecha5@gmail.com>
2024-05-09 23:54:22 +0530

    overlay issue fixed

--- components/header/settings-menu.tsx
+++ components/header/settings-menu.tsx
@@ -112,7 +112,7 @@ export default function SettingMenu(props: TProps) {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items className="absolute left-0 z-20 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             {/* <div className="px-4 py-3">
             <p className="text-sm">Signed in as</p>
             <p className="truncate text-sm font-medium text-gray-900">
