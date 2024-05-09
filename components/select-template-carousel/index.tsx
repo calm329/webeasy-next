116061c26600a4e5df2355ada9fe4560920e2fab
shubhamrakhecha5@gmail.com <shubhamrakhecha5@gmail.com>
2024-05-09 17:36:42 +0530

    save to swtich for template

--- components/select-template-carousel/index.tsx
+++ components/select-template-carousel/index.tsx
@@ -56,7 +56,7 @@ const SelectTemplateCarousel = (props: TProps) => {
         type="submit"
         className={`ml-auto  flex gap-2 rounded-md bg-indigo-600 px-3 py-2  text-sm font-semibold text-white  shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 `}
       >
-        Save
+        Switch
       </button>
     </div>
   );
