9a50fd881e763f3630fcc9d52e7fac5a765327d8
calm <calmdev0329@gmail.com>
2024-05-16 02:15:50 +0530

    site redux also complete

--- lib/api/site-api.ts
+++ lib/api/site-api.ts
@@ -1,8 +1,5 @@
-import prisma from "@/lib/prisma";
-import { getServerSession } from "next-auth";
-import { revalidateTag, unstable_cache } from "next/cache";
-import { authOptions } from "../auth";
-import { getUsernameFromPosts } from "../utils";
+import { checkSiteAvailability, createNewSite, updateSite } from "../actions";
+import { getSiteData, getSitesByUserId } from "../fetchers";
 
 class SiteApiService {
   /**
@@ -19,101 +16,14 @@ class SiteApiService {
     accessToken: string;
     userId: string;
   }) {
-    const session = await getServerSession(authOptions);
-
-    if (!session) {
-      return {
-        error: "Session not found",
-      };
-    }
-
-    const user = await prisma.user.findFirst({
-      where: {
-        email: session.user?.email,
-      },
-    });
-    try {
-      // find default template (basic template)
-      const defaultTemplate = await prisma.template.findFirst({
-        where: {
-          name: "Basic template",
-        },
-      });
-
-      if (!defaultTemplate) {
-        return {
-          error: "Default template not found",
-        };
-      }
-      const username = getUsernameFromPosts(posts);
-      const siteResponse = await prisma.site.create({
-        data: {
-          subdomain: username,
-          posts,
-          aiResult,
-          userId: user?.id,
-          templateId: defaultTemplate.id,
-        },
-      });
-
-      // create access token
-      const accessTokenResponse = await prisma.accessToken.create({
-        data: {
-          token: accessToken,
-          userId: userId,
-          siteId: siteResponse.id,
-          expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 60), // add 60 days to current date
-        },
-      });
-
-      await revalidateTag(
-        `${username}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}-metadata`,
-      );
-
-      return { subdomain: username };
-    } catch (error) {
-      console.log(error);
-
-      return { error: "Failed to create site" };
-    }
+    return createNewSite({ aiResult, posts, accessToken, userId });
   }
 
   /**
    * check if site is available
    */
   public async isAvailable(data: Partial<{ userId: string; token: string }>) {
-    try {
-      const session = await getServerSession(authOptions);
-
-      if (!session) {
-        return {
-          error: "Session not found",
-        };
-      }
-      const user = await prisma.user.findFirst({
-        where: {
-          email: session.user?.email,
-        },
-      });
-
-      const accessToken = await prisma.accessToken.findFirst({
-        where: data,
-        include: {
-          site: true,
-        },
-      });
-
-      if (accessToken) {
-        return {
-          subdomain: accessToken.site.subdomain,
-          editable: accessToken.site.userId
-            ? accessToken.site.userId === user?.id
-            : true,
-        };
-      } else {
-        return { subdomain: null, editable: true };
-      }
-    } catch (error) {}
+    return checkSiteAvailability(data);
   }
 
   /**
@@ -124,103 +34,7 @@ class SiteApiService {
     data: { [key: string]: string },
     keys: string[],
   ) {
-    const session = await getServerSession(authOptions);
-
-    try {
-      if (!session) {
-        return {
-          error: "Session not found",
-        };
-      }
-
-      const user = await prisma.user.findFirst({
-        where: {
-          email: session.user?.email,
-        },
-      });
-
-      const site = await prisma.site.findFirst({
-        where: {
-          subdomain,
-        },
-      });
-
-      if (!site) {
-        return {
-          error: "Site not found",
-        };
-      }
-
-      if (user && site.userId !== user.id) {
-        return {
-          error: "You are not authorized to update this site",
-        };
-      }
-
-      let newData: any = {
-        aiResult: JSON.parse(site.aiResult),
-      };
-
-      for (const key of keys) {
-        switch (key) {
-          case "title":
-            newData["title"] = data[key];
-            break;
-          case "description":
-            newData["description"] = data[key];
-            break;
-          case "aiResult":
-            newData["aiResult"] = data[key];
-            break;
-          case "posts":
-            newData["posts"] = data[key];
-            break;
-          case "ctaLink":
-            newData["aiResult"]["hero"]["ctaLink"] = data[key];
-            break;
-          case "cta":
-            newData["aiResult"]["hero"]["cta"] = data[key];
-            break;
-          case "businessName":
-            newData["aiResult"]["businessName"] = data[key];
-            break;
-          case "primary":
-            newData["aiResult"]["colors"]["primary"] = data[key];
-            break;
-          case "secondary":
-            newData["aiResult"]["colors"]["secondary"] = data[key];
-            break;
-          default:
-            newData["aiResult"]["hero"][key] = data[key];
-            break;
-        }
-      }
-
-      // stringify the aiResult object
-      newData.aiResult =
-        typeof newData.aiResult === "string"
-          ? newData.aiResult
-          : JSON.stringify(newData.aiResult);
-
-      if (user) {
-        newData["userId"] = user.id;
-      }
-
-      const response = await prisma.site.update({
-        where: {
-          id: site.id,
-        },
-        data: newData,
-      });
-
-      await revalidateTag(
-        `${subdomain}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}-metadata`,
-      );
-
-      return response;
-    } catch (error) {
-      console.log(error);
-    }
+    return updateSite(subdomain, data, keys);
   }
 
   /**
@@ -229,20 +43,7 @@ class SiteApiService {
    * @returns site
    */
   public async getBySubDomain(subdomain?: string) {
-    return await unstable_cache(
-      async () => {
-        return prisma.site.findFirst({
-          where: {
-            subdomain: subdomain || "",
-          },
-        });
-      },
-      [`${subdomain}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}-metadata`],
-      {
-        revalidate: 900,
-        tags: [`${subdomain}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}-metadata`],
-      },
-    )();
+    return getSiteData(subdomain);
   }
 
   /**
@@ -251,30 +52,7 @@ class SiteApiService {
    * @returns site
    */
   public async getByUserId() {
-    const session = await getServerSession(authOptions);
-
-    if (!session) {
-      return [];
-    }
-    const user = await prisma.user.findFirst({
-      where: {
-        email: session?.user?.email,
-      },
-    });
-    return await unstable_cache(
-      async () => {
-        return prisma.site.findMany({
-          where: {
-            userId: user?.id,
-          },
-        });
-      },
-      [`${process.env.NEXT_PUBLIC_ROOT_DOMAIN}-metadata`],
-      {
-        revalidate: 900,
-        tags: [`${process.env.NEXT_PUBLIC_ROOT_DOMAIN}-metadata`],
-      },
-    )();
+    return getSitesByUserId();
   }
 }
 const SiteApi = new SiteApiService();
