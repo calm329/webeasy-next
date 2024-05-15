7324f3fa80459fd53c84e076ea1783f32cac16b9
shubhamrakhecha5@gmail.com <shubhamrakhecha5@gmail.com>
2024-05-15 19:55:57 +0530

    added product-template for pulic

---
+++ app/template/product-template/page.tsx
@@ -0,0 +1,12 @@
+import ProductTemplate from "@/templates/product-template";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <ProductTemplate />
+    </div>
+  );
+};
+
+export default page;
