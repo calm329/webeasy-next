821285ba3bd9ac117696bf67ff02d65301237158
Skysparko <shubhamrakhecha5@gmail.com>
2024-05-21 23:39:36 +0530

    update domain done

---
+++ components/ui/drawer/domain-drawer.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { Drawer, DrawerContent } from "@/components/ui/drawer";
+import UpdateDomainForm from "../form/update-domain-form";
+
+type TProps = {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  subdomain:string
+};
+
+export function DomainDrawer(props: TProps) {
+  const { open, setOpen,subdomain} = props;
+  return (
+    <Drawer open={!!open} onOpenChange={setOpen}>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm px-5 pb-10">
+          <UpdateDomainForm subdomain={subdomain}/>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+}
