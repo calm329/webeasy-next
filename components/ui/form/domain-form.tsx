04954e4a7562351d325cbae5fb1eb44ad7c8c4ac
Skysparko <shubhamrakhecha5@gmail.com>
2024-05-21 23:11:35 +0530

    listing and dynamic data done for domain also preview done

--- components/ui/form/domain-form.tsx
+++ components/ui/form/domain-form.tsx
@@ -1,34 +1,61 @@
+"use client"
 import Link from "next/link";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import Search from "@/components/ui/search";
+import { useAppDispatch, useAppSelector } from "@/lib/store/hooks";
+import { fetchSitesByUser, loading as LD, sitesData as SD } from "@/lib/store/slices/site-slice";
+import { useEffect } from "react";
+import Loader from "../loader";
 export default function DomainForm() {
+  const sites = useAppSelector(SD)
+  const isLoading = useAppSelector(LD)
+  const dispatch = useAppDispatch();
+  const getData = async () => {
+    try {
+      const siteData = await dispatch(fetchSitesByUser()).unwrap();
+    } catch (error) { }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <>
       <div className="px-4 py-5 sm:p-6">
         <h2 className="text-base font-semibold leading-7 text-gray-900">
-          Current Domain
+          Current Domains
         </h2>
-        <div className="flex items-center justify-between  pt-6 sm:flex">
-          <div className=" inline-flex items-center font-medium text-indigo-600 hover:text-indigo-500  sm:flex-none">
-            <Link
-              href="https://floristpot.webeasy.ai"
-              target="_blank"
-              className="group flex gap-x-3 rounded-md py-2 pl-2 pr-3 text-sm font-semibold leading-6"
-            >
-              floristpot.webeasy.ai
-              <FaExternalLinkAlt
-                className="-ml-0.5 mr-1.5 h-4 w-4 text-indigo-600"
-                aria-hidden="true"
-              />
-            </Link>
-          </div>
-          <button
-            type="button"
-            className="font-semibold text-indigo-600 hover:text-indigo-500"
-          >
-            Update
-          </button>
+        {isLoading ? (
+        <div className="flex h-96 items-center justify-center ">
+          <Loader text="Fetching Domains" />
         </div>
+      ) : (
+        <>
+        {sites?.map((site)=>
+        <div className="flex items-center justify-between  pt-6 sm:flex" key={site.id}>
+        <div className=" inline-flex items-center font-medium text-indigo-600 hover:text-indigo-500  sm:flex-none">
+          <Link
+            href={`https://${site.subdomain}.webeasy.ai`}
+            target="_blank"
+            className="group flex gap-x-3 rounded-md py-2 pl-2 pr-3 text-sm font-semibold leading-6"
+          >
+            {site.subdomain}.webeasy.ai
+            <FaExternalLinkAlt
+              className="-ml-0.5 mr-1.5 h-4 w-4 text-indigo-600"
+              aria-hidden="true"
+            />
+          </Link>
+        </div>
+        <button
+          type="button"
+          className="font-semibold text-indigo-600 hover:text-indigo-500"
+        >
+          Update
+        </button>
+      </div>
+        )}
+        </>
+        )}
       </div>
       <div className="px-4 py-4 sm:px-6 sm:py-5">
         <h2 className="text-base font-semibold leading-7 text-gray-900">
