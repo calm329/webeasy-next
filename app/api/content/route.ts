403ee8e1fe06481f8014b92b4a8c884f9e2f4ec0
calm <calmdev0329@gmail.com>
2024-05-24 21:05:57 +0530

    regenerate fixed

--- app/api/content/route.ts
+++ app/api/content/route.ts
@@ -37,13 +37,43 @@ export async function POST(request: NextRequest) {
                 Make the list of services the most important ones identified from the instagram posts.
         
                 {
-                  "businessName": "*Name of the business inferred from all the content*",
-                  "heroImagePrompt": "*Create a prompt for dall-e-3 to create a hero image to represent the business and content from the instagram posts in a simple above the fold style*",
+                  "banner": {
+                    "businessName": "*Name of the business inferred from all the content*",
+                    "button": {
+                      "show":true,
+                      "list": [
+                        {
+                          "name": "button1",
+                          "label": "*insert call to action here, it should only be a couple of words long*",
+                          "type": "External"
+                        }
+                      ]
+                    },
+                    "logo": {
+                      "link":  "https://xhq5zxhb2o7dgubv.public.blob.vercel-storage.com/2weWEVnPETmQLpQx52_W1-Ofz4wnOvkqM6307M1pfxfkLAZXXBbX.jpeg",
+                      "alt": "",
+                      "show": true
+                    }
+                  },
                   "hero": {
+                    "button": {
+                      "show": true,
+                      "list": [
+                        {
+                          "name": "button1",
+                          "label": "*insert call to action here, it should only be a couple of words long*",
+                          "type": "External"
+                        }
+                      ]
+                    },
+                    "image": {
+                      "heroImagePrompt": "*Create a prompt for dall-e-3 to create a hero image to represent the business and content from the instagram posts in a simple above the fold style*",
+                      "imageId": "*The id of the post that best matches the heading and subheading*",
+                      "alt": "",
+                      "show": true
+                    },
                     "heading": "*insert heading here*",
-                    "subheading": "*insert subheading here*",
-                    "cta": "*insert call to action here, it should only be a couple of words long*",
-                    "imageId": "*The id of the post that best matches the heading and subheading*"
+                    "subheading": "*insert subheading here*"
                   },
                   "services": {
                     "title": "*type of services title Services or Features*",
