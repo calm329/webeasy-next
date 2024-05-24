4f91ceaef741fa6df173ef17a5673d29b89f0648
Skysparko <shubhamrakhecha5@gmail.com>
2024-05-25 00:04:39 +0530

    build error fixed

--- templates/basic-template/index.tsx
+++ templates/basic-template/index.tsx
@@ -5,6 +5,7 @@ import CTA from "@/components/cta";
 import TopBar from "@/components/top-bar";
 import { generateUniqueId } from "@/lib/utils/function";
 import { TBanner, TColors, THero } from "@/types";
+import Link from "next/link";
 
 type BasicTemplateProps = {
   banner: TBanner;
@@ -21,7 +22,48 @@ export default function BasicTemplate(props: BasicTemplateProps) {
     <>
       <section className="bg-white py-6">
         <div className={`container mx-auto px-4`}>
-          <TopBar banner={banner} colors={colors} />
+          <div className="flex items-center justify-between rounded-full border border-gray-100 bg-gray-100 px-6 py-3.5 max-sm:flex-col max-sm:gap-5">
+            <div className="w-auto">
+              <div className="flex flex-wrap items-center">
+                <div
+                  className="text-black-300 flex w-auto items-center gap-2 text-xl font-medium"
+                  style={{ color: colors.primary }}
+                >
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  {banner.logo && (
+                    <Image
+                      src={banner.logo.link}
+                      alt={banner.logo.alt}
+                      className={`h-8 w-auto `}
+                      height={32}
+                      width={200}
+                    />
+                  )}
+                  <Link href="#">{banner.businessName}</Link>
+                </div>
+              </div>
+            </div>
+            <div className="w-auto">
+              <div className="flex flex-wrap items-center">
+                <div className="w-auto lg:block">
+                  <div className="-m-2 flex flex-wrap">
+                    <div className={`flex w-full gap-5 p-2 md:w-auto`}>
+                      {banner.button.list.map((data, i) => (
+                        <div key={i}>
+                          <CTA
+                            text={data.label}
+                            bgColor={colors.secondary}
+                            link={data.value ?? ""}
+                            external={data.type === "External"}
+                          />
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
       <section className="overflow-hidden bg-gray-50 py-10">
@@ -47,8 +89,8 @@ export default function BasicTemplate(props: BasicTemplateProps) {
                             <CTA
                               text={button.label}
                               bgColor={colors.secondary}
-                              link={button.value ??""}
-                              external={button.type ==="External"}
+                              link={button.value ?? ""}
+                              external={button.type === "External"}
                             />
                           </div>
                         ))}
