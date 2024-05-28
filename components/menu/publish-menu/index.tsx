f7035637fb237fa336a4599465accb469b394f9f
calm <calmdev0329@gmail.com>
2024-05-28 21:33:58 +0530

    redux persisted and changes done for preview

--- components/menu/publish-menu/index.tsx
+++ components/menu/publish-menu/index.tsx
@@ -71,13 +71,14 @@ export default function PublishMenu(props: TProps) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items
+      <Menu.Items
           className={`absolute left-0 z-10 -mr-1 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${isMobile && "-top-28 left-auto right-0"}`}
         >
           <Menu.Item>
             {({ active }) => (
               <Link
                 href={"/preview"}
+                target="_blank"
                 className={classNames(
                   active ? "bg-gray-100" : "",
                   "block px-4 py-2 text-sm text-gray-700",
