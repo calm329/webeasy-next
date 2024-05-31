fa70def955b7178b451ffb7dfb26e515c46fb17a
Skysparko <shubhamrakhecha5@gmail.com>
2024-06-01 00:30:43 +0530

    view done

--- templates/basic-template/basic-template-csr.tsx
+++ templates/basic-template/basic-template-csr.tsx
@@ -134,7 +134,7 @@ export default function BasicTemplate(props: BasicTemplateProps) {
       }}>
         <h2 className="sr-only">Posts</h2>
        { appState.iPosts.show && 
-        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <div className="flex flex-wrap gap-5">
           {posts.list.map((post,i) => (
             posts.limit > i &&
             <PostCard
