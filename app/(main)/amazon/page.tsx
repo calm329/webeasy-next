6aeb681b26466c81f53812759c1224f44bf3da62
calm <calmdev0329@gmail.com>
2024-06-08 02:43:57 +0530

    added header in amazon page

--- app/(main)/amazon/page.tsx
+++ app/(main)/amazon/page.tsx
@@ -1,9 +1,10 @@
 import SiteHeader from "@/components/header";
 import ProductTemplate from "@/templates/product-template";
 import React from "react";
+// import { Header } from '../../../components/header/blue-based-header';
 
 const page = () => {
-  return <ProductTemplate />;
+  return <><SiteHeader showNavigation={false}/><ProductTemplate /></>;
 };
 
 export default page;
