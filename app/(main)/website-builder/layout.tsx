220666d34b7a94763348d4e00c1200332e70f4d7
calm <calmdev0329@gmail.com>
2024-06-11 21:51:36 +0530

    placeholder text and sections layout design changed for mobile

--- app/(main)/website-builder/layout.tsx
+++ app/(main)/website-builder/layout.tsx
@@ -6,20 +6,24 @@ import { usePathname } from "next/navigation";
 type TSectionObject = Array<{
     link:string,
     text:string,
+    shortText:string
 }>;
 
 const sections: TSectionObject = [
     {
         link:"/website-builder",
-        text:"Build a website from scratch"
+        text:"Build a website from scratch",
+        shortText:"Scratch"
     },
     {
         link:"/website-builder/instagram",
-        text:"Build a website from Instagram"
+        text:"Build a website from Instagram",
+           shortText:"Instagram"
     },
     {
         link:"/website-builder/amazon",
-        text:"Build an Amazon landing page"
+        text:"Build an Amazon landing page",
+           shortText:"Amazon"
     }
 ];
 
@@ -32,9 +36,12 @@ export default function Layout({
   return (
     <div className="max-w-7xl mx-auto">
       <SiteHeader showNavigation={true} />
-      <h1 className="mx-auto  p-10 text-2xl font-bold">
-        Establish your online presence in seconds with WebEasy.ai
-      </h1>
+      <div className="flex flex-col justify-center items-center p-10 gap-5">
+        <h1 className="mx-auto   text-2xl font-bold">
+          Establish your online presence in seconds with WebEasy.ai
+        </h1>
+        <p className="mx-auto text-xl font-semibold sm:hidden">Build a website from </p>
+      </div>
       <div className="">
         <div className="border-b border-gray-200 ">
           <nav className=" flex " aria-label="Tabs">
@@ -45,7 +52,8 @@ export default function Layout({
                 className={`group inline-flex items-center border-b-2  px-1 py-4 text-sm font-medium ${pathname === section.link ? "border-indigo-500 text-indigo-600" : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"} w-1/2 justify-center gap-2`}
                 // onClick={() => setSelectedSection(section)}
               >
-                <span>{section.text}</span>
+                <span className="max-sm:hidden">{section.text}</span>
+                <span className="sm:hidden">{section.shortText}</span>
               </Link>
             ))}
           </nav>
