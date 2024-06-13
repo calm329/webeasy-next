8baec27660183078d1ba16998f8102f85a5ce16b
calm <calmdev0329@gmail.com>
2024-06-14 01:25:34 +0530

    header done for general

--- components/header/general-header.tsx
+++ components/header/general-header.tsx
@@ -6,26 +6,40 @@ import { Container } from "../container/nested-container";
 import { Button } from "../ui/button/template-button";
 import { TBanner, TColors, TSection } from "@/types";
 import { Dispatch, SetStateAction } from "react";
+import { updateAppState, appState as AS } from "@/lib/store/slices/site-slice";
+import { useAppDispatch, useAppSelector } from "@/lib/store/hooks";
 type TProps = {
   banner: TBanner;
   setIsOpen?: Dispatch<SetStateAction<boolean>>;
   setSection?: Dispatch<SetStateAction<TSection>>;
   editable?: boolean;
-  showForm?:{
-    form:string,
-    edit:string,
-    show: boolean,
+  showForm?: {
+    form: string;
+    edit: string;
+    show: boolean;
   };
-  setShowForm?:React.Dispatch<React.SetStateAction<{
-    form:string,
-    edit:string,
-    show: boolean,
-  }>>;
-  colors:TColors
+  setShowForm?: React.Dispatch<
+    React.SetStateAction<{
+      form: string;
+      edit: string;
+      show: boolean;
+    }>
+  >;
+  colors: TColors;
 };
 
 export function Header(props: TProps) {
-  const { banner, colors,editable,setIsOpen,setSection,setShowForm,showForm} = props;
+  const {
+    banner,
+    colors,
+    editable,
+    setIsOpen,
+    setSection,
+    setShowForm,
+    showForm,
+  } = props;
+  const dispatch = useAppDispatch();
+  const appState = useAppSelector(AS);
   return (
     <>
       <header
@@ -49,35 +63,86 @@ export function Header(props: TProps) {
               position:
                 "var(--header-inner-position)" as React.CSSProperties["position"],
             }}
-
+            onClick={() => {
+              if (editable && setIsOpen && setSection && setShowForm) {
+                setSection("Banner");
+                setIsOpen(true);
+                setShowForm({
+                  show: false,
+                  edit: "",
+                  form: "",
+                });
+                dispatch(
+                  updateAppState({ ...appState, openedSlide: "Customize" }),
+                );
+              }
+            }}
           >
             <div className="relative flex gap-4">
               <div className="flex flex-1 justify-end md:justify-center">
                 <nav className="pointer-events-auto block w-full">
-                  <div className="flex w-full justify-between rounded-full border bg-white/90 px-10 py-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur-lg dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 max-md:px-5 max-sm:flex-col max-sm:gap-5 max-sm:rounded items-center">
+                  <div className="flex w-full items-center justify-between rounded-full border bg-white/90 px-10 py-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur-lg dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 max-md:px-5 max-sm:flex-col max-sm:gap-5 max-sm:rounded">
                     <div
                       className="flex items-center gap-5 max-md:gap-2"
                       style={{ color: colors.primary }}
                     >
-                      <Image
-                        src={banner.logo.link ?? ""}
-                        alt={banner.logo.alt??""}
-                        height={100}
-                        width={100}
-                      />
-                      <h2 className="text-xl max-md:text-base">
-                        {banner.businessName}
-                      </h2>
-                    </div>
-                    {banner.button.list.map((data, i) => (
-                      <div key={i}>
-                        <Button
-                          href={data.value?? "#"}
-                          text={data.label}
-                          bgColor={colors.secondary}
+                      {banner.logo.show && (
+                        <Image
+                          src={banner.logo.link ?? ""}
+                          alt={banner.logo.alt ?? ""}
+                          height={100}
+                          width={100}
+                          className={`${editable && "rounded border-2 border-transparent hover:border-indigo-500"}`}
+                          onClick={() => {
+                            if (setIsOpen && setSection && setShowForm) {
+                              setSection("Banner");
+                              setIsOpen(true);
+                              // setFocusedField("logo");
+                              setShowForm({
+                                form: "",
+                                edit: "",
+                                show: false,
+                              });
+                            }
+                          }}
                         />
-                      </div>
-                    ))}
+                      )}
+
+                      <span
+                        className={`${editable && "rounded border-2 border-transparent hover:border-indigo-500"}`}
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          if (setIsOpen && setSection && setShowForm) {
+                            setSection("Banner");
+                            setIsOpen(true);
+                            setShowForm({
+                              form: "",
+                              edit: "",
+                              show: false,
+                            });
+                            dispatch(
+                              updateAppState({
+                                ...appState,
+                                focusedField: "businessName",
+                                openedSlide: "Customize",
+                              }),
+                            );
+                          }
+                        }}
+                      >
+                        {banner.businessName}
+                      </span>
+                    </div>
+                    {banner.button.show &&
+                      banner.button.list.map((data, i) => (
+                        <div key={i}>
+                          <Button
+                            href={data.value ?? "#"}
+                            text={data.label}
+                            bgColor={colors.secondary}
+                          />
+                        </div>
+                      ))}
                   </div>
                 </nav>
               </div>
