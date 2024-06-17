4f3065a5b91451beaec8f0cea321365f02c32533
Skysparko <shubhamrakhecha5@gmail.com>
2024-06-17 23:44:25 +0530

    text updated in business location

--- components/ui/form/custom-website-form.tsx
+++ components/ui/form/custom-website-form.tsx
@@ -382,7 +382,7 @@ const CustomWebsiteForm = () => {
             <input
               type="text"
               {...register("location")}
-              placeholder="We'd love to learn more about your business! Could you please provide some details?"
+              placeholder="Enter your business location"
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
             {errors.location && (
