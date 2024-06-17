766d23e809d0031eb961c82be32dc014e5d79b7a
Skysparko <shubhamrakhecha5@gmail.com>
2024-06-17 23:51:35 +0530

    selected font on top done

--- components/font-picker/index.tsx
+++ components/font-picker/index.tsx
@@ -3,36 +3,62 @@ import axios from "axios";
 import { appState as AS, updateAppState } from "@/lib/store/slices/site-slice";
 import { useAppDispatch, useAppSelector } from "@/lib/store/hooks";
 
-const fonts = [
-  "Roboto",
-  "Open Sans",
-  "Montserrat",
-  "Lato",
-  "Poppins",
-  "Source Sans Pro",
-  "Raleway",
-  "Noto Sans",
-  "Inter",
-  "Roboto Slab",
-  "Merriweather",
-  "Playfair Display",
-  "Oswald",
-  "Nunito",
-  "Inter Rubik",
-  "PT Serif",
-  "Ubuntu",
-  "Barlow",
-  "Lobster",
-  "LoRa",
-  "Monospace",
-  "Muli",
-  "Alegreya",
-  "Work Sans"
-];
+// const fonts = [
+//   "Roboto",
+//   "Open Sans",
+//   "Montserrat",
+//   "Lato",
+//   "Poppins",
+//   "Source Sans Pro",
+//   "Raleway",
+//   "Noto Sans",
+//   "Inter",
+//   "Roboto Slab",
+//   "Merriweather",
+//   "Playfair Display",
+//   "Oswald",
+//   "Nunito",
+//   "Inter Rubik",
+//   "PT Serif",
+//   "Ubuntu",
+//   "Barlow",
+//   "Lobster",
+//   "LoRa",
+//   "Monospace",
+//   "Muli",
+//   "Alegreya",
+//   "Work Sans"
+// ];
 
 const FontPicker = () => {
   const appState = useAppSelector(AS);
   const dispatch = useAppDispatch();
+  const [fonts, setFonts] = useState([
+    "Roboto",
+    "Open Sans",
+    "Montserrat",
+    "Lato",
+    "Poppins",
+    "Source Sans Pro",
+    "Raleway",
+    "Noto Sans",
+    "Inter",
+    "Roboto Slab",
+    "Merriweather",
+    "Playfair Display",
+    "Oswald",
+    "Nunito",
+    "Inter Rubik",
+    "PT Serif",
+    "Ubuntu",
+    "Barlow",
+    "Lobster",
+    "LoRa",
+    "Monospace",
+    "Muli",
+    "Alegreya",
+    "Work Sans",
+  ]);
 
   useEffect(() => {
     const WebFontLoader = require("webfontloader");
@@ -42,6 +68,17 @@ const FontPicker = () => {
       },
     });
   }, [fonts]);
+
+  useEffect(() => {
+    if (appState.selectedFont) {
+      setFonts((prevFonts) => {
+        const newFonts = prevFonts.filter(
+          (font) => font !== appState.selectedFont,
+        );
+        return [appState.selectedFont, ...newFonts];
+      });
+    }
+  }, [appState.selectedFont]);
 
   return (
     <div className=" flex flex-col gap-5">
