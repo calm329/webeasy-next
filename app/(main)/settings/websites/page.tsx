67f0a12677b61c8c8fd4464a8ea30423ff892e47
Skysparko <shubhamrakhecha5@gmail.com>
2024-06-27 02:41:23 +0530

    tabs fixed

--- app/(main)/settings/websites/page.tsx
+++ app/(main)/settings/websites/page.tsx
@@ -10,8 +10,9 @@ import { MdAnalytics, MdDomain } from "react-icons/md";
 
 const tabs = [
   { name: "My Websites", href: "/settings/websites", icon: GlobeAltIcon },
-  { name: "Domain", href: "/settings/domain", icon: MdDomain },
   { name: "Analytics", href: "#", icon: MdAnalytics },
+  { name: "Domains", href: "/settings/domain", icon: MdDomain },
+
 ];
 
 export default function MyWebsites() {
@@ -31,7 +32,7 @@ export default function MyWebsites() {
               <Link
                 href={tab.href}
                 key={tab.name}
-                className={`group inline-flex items-center border-b-2  px-1 py-4 text-sm font-medium ${pathname === tab.href ? "border-indigo-500 text-indigo-600" : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"} w-1/2 justify-center gap-2`}
+                className={`group w-fit px-5 inline-flex items-center border-b-2   py-4 text-sm font-medium ${pathname === tab.href ? "border-indigo-500 text-indigo-600" : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"} w-1/2 justify-center gap-2`}
                 // onClick={() => {
                 //   setSelectedSection(section.name);
                 //   setPage(1);
