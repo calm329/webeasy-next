1824bb4b0062fca8454c0cf443f9c2a5ee21430f
Skysparko <shubhamrakhecha5@gmail.com>
2024-07-09 08:34:46 +0530

    contact in site

--- lib/store/slices/site-slice.ts
+++ lib/store/slices/site-slice.ts
@@ -35,6 +35,10 @@ const initialSite: AppState = {
   status: "Loading",
   iPosts: { limit: 20, show: true, list: [], showHash: true },
   aiContent: {
+    contact:{
+      title:"",
+      description:"",
+    },
     blog:{
       title: "",
       description: "",
