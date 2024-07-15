e72ad19927d6ebf9e19757c2f80aa908e228a627
Skysparko <shubhamrakhecha5@gmail.com>
2024-07-16 01:21:03 +0530

    added image picker

--- components/ui/form/images-listing.tsx
+++ components/ui/form/images-listing.tsx
@@ -1,18 +1,19 @@
 import React, { SetStateAction, useEffect, useState } from "react";
-import { appState as AS } from "@/lib/store/slices/site-slice";
-import { useAppSelector } from "@/lib/store/hooks";
+import { appState as AS, updateAppState } from "@/lib/store/slices/site-slice";
+import { useAppDispatch, useAppSelector } from "@/lib/store/hooks";
 import { getAllUnsplashImages } from "@/lib/utils/function";
 import Image from "next/image";
 
 type TProps = {
   selectedImage: string;
-  setOpen: React.Dispatch<React.SetStateAction<boolean>>
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 const ImagesListing = (props: TProps) => {
-  const { selectedImage } = props;
+  const { selectedImage, setOpen } = props;
   const appState = useAppSelector(AS);
   const [images, setImages] = useState([]);
+  const dispatch = useAppDispatch();
   useEffect(() => {
     getAllUnsplashImages(appState.aiContent?.businessType ?? "").then(
       (images) => {
@@ -20,11 +21,43 @@ const ImagesListing = (props: TProps) => {
       },
     );
   }, [appState]);
-  console.log("images",images)
+  console.log("images", images);
   return (
-    <div className="grid grid-cols-3 gap-10">
+    <div className="mt-10 grid grid-cols-3 gap-10">
       {images?.map((data: any, i) => (
-        <Image alt="" src={data.urls.full} key={i} height={200} width={200} />
+        <div
+          key={i}
+          className="relative h-44 cursor-pointer hover:border hover:border-indigo-600 hover:shadow-xl"
+          onClick={() => {
+            dispatch(
+              updateAppState({
+                ...appState,
+                aiContent: {
+                  ...appState.aiContent,
+                  gallery: {
+                    ...appState.aiContent.gallery,
+                    list: appState.aiContent.gallery.list.map((image, i) => {
+                      if (parseInt(selectedImage) === i) {
+                        return data.urls.small;
+                      } else {
+                        return image;
+                      }
+                    }),
+                  },
+                },
+              }),
+            );
+            setOpen(false);
+          }}
+        >
+          <Image
+            alt=""
+            src={data.urls.small}
+            height={200}
+            width={200}
+            className=" absolute h-full w-full object-cover"
+          />
+        </div>
       ))}
     </div>
   );
