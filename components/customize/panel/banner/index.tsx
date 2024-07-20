54202ae4f878bb66741a19c1ee661890a9255ba4
Skysparko <shubhamrakhecha5@gmail.com>
2024-07-20 21:23:32 +0530

    businessName added

--- components/customize/panel/banner/index.tsx
+++ components/customize/panel/banner/index.tsx
@@ -297,27 +297,27 @@ const BannerContent = (props: TProps) => {
                       </div>
                     </div>
                   );
-                // case "businessName":
-                //   return (
-                //     <div className="flex flex-col border-t pt-5">
-                //       <div className="flex  items-center justify-between text-sm font-medium leading-6 text-gray-900 ">
-                //         <label htmlFor="businessName" className="block">
-                //           Business Name
-                //         </label>
-                //       </div>
+                case "businessName":
+                  return (
+                    <div className="flex flex-col border-t pt-5">
+                      <div className="flex  items-center justify-between text-sm font-medium leading-6 text-gray-900 ">
+                        <label htmlFor="businessName" className="block">
+                          Business Name
+                        </label>
+                      </div>
 
-                //       <input
-                //         type="text"
-                //         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                //         placeholder={"Enter your business name"}
-                //         onChange={(e) => {
-                //           handleChange(data, e.target.value);
-                //         }}
-                //         ref={inputRef}
-                //         value={appState?.aiContent?.banner?.businessName}
-                //       />
-                //     </div>
-                //   );
+                      <input
+                        type="text"
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        placeholder={"Enter your business name"}
+                        onChange={(e) => {
+                          handleChange(data, e.target.value);
+                        }}
+                        ref={inputRef}
+                        value={appState?.aiContent?.banner?.businessName}
+                      />
+                    </div>
+                  );
 
                 case "button":
                   return (
