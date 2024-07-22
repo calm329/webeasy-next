b8592fc470be36eda325c3927cbb64b8fa925486
Skysparko <shubhamrakhecha5@gmail.com>
2024-07-22 22:36:26 +0530

    all added git

--- components/ui/form/websites-form.tsx
+++ components/ui/form/websites-form.tsx
@@ -1,11 +1,7 @@
-"use client";
 import Loader from "@/components/ui/loader";
 import { getAccessTokenBySiteId, getSitesByUserId } from "@/lib/fetchers";
 import { useAppDispatch, useAppSelector } from "@/lib/store/hooks";
 import { fetchAccessToken } from "@/lib/store/slices/accesstoken-slice";
-import { getUsernameFromPosts } from "@/lib/utils";
-import Image from "next/image";
-import Link from "next/link";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { FaExternalLinkAlt, FaInstagram } from "react-icons/fa";
@@ -33,14 +29,22 @@ import {
 import DeleteModal from "../modal/delete-modal";
 import { DeleteDrawer } from "../drawer/delete-drawer";
 import { useMediaQuery } from "usehooks-ts";
-import { setSelectedTemplate, TemplatesData as TD } from "@/lib/store/slices/template-slice";
+import {
+  setSelectedTemplate,
+  TemplatesData as TD,
+} from "@/lib/store/slices/template-slice";
+import Image from "next/image";
+import Link from "next/link";
 
 type TSectionObject = Array<{
   logo: React.ReactNode;
   name: TSectionName;
-  // href: string;
 }>;
 const sections: TSectionObject = [
+  {
+    logo: "",
+    name: "All",
+  },
   {
     logo: <FaInstagram />,
     name: "Instagram",
@@ -54,7 +58,7 @@ const sections: TSectionObject = [
     name: "Custom",
   },
 ];
-type TSectionName = "Instagram" | "Amazon" | "Custom";
+type TSectionName = "Instagram" | "Amazon" | "Custom" | "All";
 
 type TSites = Array<{
   id: string;
@@ -82,7 +86,7 @@ const sortByCreatedAtDescending = (sites: TSites) => {
 
 export default function WebsitesForm() {
   const [selectedSection, setSelectedSection] =
-    useState<TSectionName>("Instagram");
+    useState<TSectionName>("All");
   const searchParams = useSearchParams();
   const router = useRouter();
   const sites = useAppSelector(SD);
@@ -93,26 +97,28 @@ export default function WebsitesForm() {
   const [paginatedData, setPaginatedData] = useState<TSites>(null);
   const [isDeleting, setIsDeleting] = useState(false);
   const [selectedItemId, setSelectedItemId] = useState("");
-  const pathname = usePathname()
-  const templates = useAppSelector(TD)
-  const appState = useAppSelector(AS)
+  const pathname = usePathname();
+  const templates = useAppSelector(TD);
+  const appState = useAppSelector(AS);
+
   useEffect(() => {
     if (sites) {
+      const filteredSites =
+        selectedSection === "All"
+          ? sites
+          : sites.filter((site) => site.type === selectedSection);
+      const sortedSites = sortByCreatedAtDescending(filteredSites)??[];
       const startIndex = (page - 1) * dataPerPage;
       const endIndex = startIndex + dataPerPage;
-      setPaginatedData(
-        sites
-          .filter((site) => site.type === selectedSection)
-          .slice(startIndex, endIndex),
-      );
+      setPaginatedData(sortedSites.slice(startIndex, endIndex));
     }
-  }, [page, sites, selectedSection,isLoading]);
+  }, [page, sites, selectedSection, isLoading]);
 
   const getData = async () => {
     try {
       const siteData = await dispatch(fetchSitesByUser()).unwrap();
       console.log("siteData", siteData);
-    } catch (error) { }
+    } catch (error) {}
   };
 
   useEffect(() => {
@@ -131,27 +137,16 @@ export default function WebsitesForm() {
 
         router.replace(`/auth?${newURLSearchParams.toString()}`);
       }
-    } catch (error) { }
+    } catch (error) {}
   };
-  useEffect(() => {
-    if (sites) {
-      const filteredSites = sites.filter(
-        (site) => site.type === selectedSection,
-      );
-      const sortedSites = sortByCreatedAtDescending(filteredSites);
-      const startIndex = (page - 1) * dataPerPage;
-      const endIndex = startIndex + dataPerPage;
-      if (sortedSites) {
-        setPaginatedData(sortedSites.slice(startIndex, endIndex));
-      }
-    }
-  }, [page, sites, selectedSection]);
 
   const totalPages = Math.ceil(
-    (sites?.filter((site) => site.type === selectedSection).length || 0) /
-    dataPerPage,
+    (sites?.filter((site) => selectedSection === "All" || site.type === selectedSection).length || 0) /
+      dataPerPage,
   );
+
   const matches = useMediaQuery("(min-width: 768px)");
+
   return (
     <div className="">
       {matches ? (
@@ -169,11 +164,11 @@ export default function WebsitesForm() {
       )}
       <div className="">
         <div className="border-b border-gray-200 ">
-          <nav className=" flex " aria-label="Tabs">
+          <nav className="flex" aria-label="Tabs">
             {sections.map((section) => (
               <button
                 key={section.name}
-                className={`group inline-flex items-center border-b-2  px-1 py-4 text-sm font-medium ${selectedSection === section.name ? "border-indigo-500 text-indigo-600" : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"} w-1/2 justify-center gap-2`}
+                className={`inline-flex items-center border-b-2 px-1 py-4 text-sm font-medium ${selectedSection === section.name ? "border-indigo-500 text-indigo-600" : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"} w-1/2 justify-center gap-2`}
                 onClick={() => {
                   setSelectedSection(section.name);
                   setPage(1);
@@ -193,81 +188,86 @@ export default function WebsitesForm() {
       ) : (
         <div className="flex flex-wrap gap-10 px-5 pt-10">
           {paginatedData?.map(
-            (site) =>
-              site.type === selectedSection && (
-                <div
-                  key={site.id}
-                  className=" relative flex max-w-80 flex-col items-center justify-between rounded-lg border   shadow"
+            (site) => (
+              <div
+                key={site.id}
+                className="relative flex max-w-80 flex-col items-center justify-between rounded-lg border shadow"
+              >
+                <button
+                  className="z-1 absolute right-2 top-2 rounded-full bg-white p-2"
+                  onClick={() => {
+                    setIsDeleting(true);
+                    setSelectedItemId(site.id);
+                  }}
                 >
-                  <button
-                    className="z-1 absolute right-2 top-2 rounded-full bg-white p-2"
-                    onClick={() => {
-                      setIsDeleting(true);
-                      setSelectedItemId(site.id);
-                      // dispatch(deleteSite({ id: site.id }));
-                    }}
-                  >
-                    <BsTrash3 color="red" />
-                  </button>
-                  <div className="min-h-80 rounded-t-lg flex justify-center items-center h-full">
-                    <Image
-                      src={site.type === "Amazon" ? JSON.parse(site?.aiResult)?.images?.primary?.Large?.URL ?? "" : JSON.parse(site?.aiResult)?.hero?.image?.imageUrl ?? ""}
-                      height={200}
-                      width={500}
-                      className="contain "
-                      alt=""
-                    />
+                  <BsTrash3 color="red" />
+                </button>
+                <div className="flex h-full min-h-80 items-center justify-center rounded-t-lg">
+                  <Image
+                    src={
+                      site.type === "Amazon"
+                        ? JSON.parse(site?.aiResult)?.images?.primary?.Large?.URL ?? ""
+                        : JSON.parse(site?.aiResult)?.hero?.image?.imageUrl ?? ""
+                    }
+                    height={200}
+                    width={500}
+                    className="contain"
+                    alt=""
+                  />
+                </div>
+                <div className="flex flex-col gap-5 p-5">
+                  <div className="flex flex-col gap-2 ">
+                    <h2 className="line-clamp-1 text-xl font-bold">
+                      {site?.title}
+                    </h2>
+                    <p className="text-sm">{site?.description}</p>
                   </div>
-                  <div className="flex flex-col gap-5 p-5">
-                    <div className="flex flex-col gap-2 ">
-                      <h2 className="line-clamp-1 text-xl font-bold">
-                        {site?.title}
-                      </h2>
-                      <p className="  text-sm">
-                        {site?.description}
-                      </p>
-                    </div>
 
-                    <div className="flex gap-5">
-                      <button
-                        className="text-500 inline-flex w-full items-center justify-center gap-x-1.5 rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white hover:bg-indigo-500"
-                        onClick={() => {
-                          if (templates) {
-                            dispatch(setSelectedTemplate(templates[0]))
-                          }
-                          dispatch(updateAppState({
+                  <div className="flex gap-5">
+                    <button
+                      className="text-500 inline-flex w-full items-center justify-center gap-x-1.5 rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white hover:bg-indigo-500"
+                      onClick={() => {
+                        if (templates) {
+                          dispatch(setSelectedTemplate(templates[0]));
+                        }
+                        dispatch(
+                          updateAppState({
                             ...appState,
-                            view: "Desktop"
-                          }))
-                          switch (site.type) {
-                            case "Custom":
-                              router.push("/custom/" + site.id);
-                              break;
-                            case "Instagram":
-                              redirectToAuth(site.id);
-                              break;
-                            case "Amazon":
-                              router.push("/amazon/" + site.id);
-                              break;
-                          }
-                        }}
-                      >
-                        <FaEdit />
-                        Edit
-                      </button>
-                      <Link
-                        href={site.type === "Amazon" ? "/preview/amazon?preview_site=" + site.id : "/preview?preview_site=" + site.id}
-                        target="_blank"
-                        className="text-500 inline-flex w-full items-center justify-center gap-x-1.5 rounded-md border-2 border-gray-400 bg-white px-5 py-1 text-sm font-semibold hover:bg-gray-100"
-                      >
-
-                        <FaExternalLinkAlt />
-                        Preview
-                      </Link>
-                    </div>
+                            view: "Desktop",
+                          }),
+                        );
+                        switch (site.type) {
+                          case "Custom":
+                            router.push("/custom/" + site.id);
+                            break;
+                          case "Instagram":
+                            redirectToAuth(site.id);
+                            break;
+                          case "Amazon":
+                            router.push("/amazon/" + site.id);
+                            break;
+                        }
+                      }}
+                    >
+                      <FaEdit />
+                      Edit
+                    </button>
+                    <Link
+                      href={
+                        site.type === "Amazon"
+                          ? "/preview/amazon?preview_site=" + site.id
+                          : "/preview?preview_site=" + site.id
+                      }
+                      target="_blank"
+                      className="text-500 inline-flex w-full items-center justify-center gap-x-1.5 rounded-md border-2 border-gray-400 bg-white px-5 py-1 text-sm font-semibold hover:bg-gray-100"
+                    >
+                      <FaExternalLinkAlt />
+                      Preview
+                    </Link>
                   </div>
                 </div>
-              ),
+              </div>
+            )
           )}
           <Pagination>
             <PaginationContent>
