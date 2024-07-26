f07d8f5533e2085f72b8c72678ea70851b8b8ed9
Skysparko <shubhamrakhecha5@gmail.com>
2024-07-27 02:24:09 +0530

    newsletter done

--- components/sections/newsletter/index.tsx
+++ components/sections/newsletter/index.tsx
@@ -6,9 +6,10 @@ import CustomContent from "@/lib/content/custom";
 import { TFields, TSection, TSectionsType } from "@/types";
 import { Dispatch, SetStateAction } from "react";
 
-import {Skeleton} from "@/components/ui/skeleton"; // Import the Skeleton component here
-import { appState as AS, updateAppState } from '@/lib/store/slices/site-slice';
+import { Skeleton } from "@/components/ui/skeleton"; // Import the Skeleton component here
+import { appState as AS, updateAppState } from "@/lib/store/slices/site-slice";
 import EditComponent from "@/components/edit-component";
+import { sectionsData as SD } from '@/lib/store/slices/section-slice';
 
 type TProps = {
   editable?: boolean;
@@ -31,7 +32,7 @@ type TProps = {
     show: boolean;
   };
 
-  id:string
+  id: string;
 };
 
 export default function NewsLetterSection(props: TProps) {
@@ -62,7 +63,7 @@ export default function NewsLetterSection(props: TProps) {
           ...appState,
           focusedField: field,
           openedSlide: "Customize",
-        })
+        }),
       );
     }
   };
@@ -83,23 +84,73 @@ export default function NewsLetterSection(props: TProps) {
     });
   }, []);
 
+  const sections = useAppSelector(SD);
+  
+  // Find the section by ID and get the variation
+  const section = sections.find((section) => section.id === id);
+  const variation = section?.variation || 1;
+
+  const styles: {
+    [key: number]: {
+      container: string;
+      listContainer: string;
+      contentContainer: string;
+      listCard: string;
+    };
+  } = {
+    1: {
+      contentContainer: "",
+      listContainer: "",
+      container: "mx-auto flex justify-between items-center max-w-7xl  gap-10 px-6 flex-wrap  lg:gap-8 lg:px-8",
+      listCard: "",
+    },
+    2: {
+      contentContainer: "",
+      listContainer: "",
+      container: "mx-auto max-w-7xl flex-row-reverse gap-10 px-6 flex justify-between items-center flex-wrap  lg:gap-8 lg:px-8",
+      listCard: "",
+    },
+    3: {
+      contentContainer: "",
+      listContainer: "",
+      container: "mx-auto max-w-7xl flex-col-reverse gap-10 px-6 flex justify-between items-center flex-wrap  lg:gap-8 lg:px-8",
+      listCard: "",
+    },
+    4: {
+      contentContainer: "",
+      listContainer: "",
+      container: "mx-auto max-w-7xl flex-col gap-10 px-6 flex justify-between items-center flex-wrap  lg:gap-8 lg:px-8",
+      listCard: "",
+    },
+    5: {
+      contentContainer: "",
+      listContainer: "",
+      container: "mx-auto grid max-w-7xl grid-cols-1 gap-10 px-6 lg:grid-cols-12 lg:gap-8 lg:px-8",
+      listCard: "",
+    },
+  };
+
+  const { container, listContainer, listCard, contentContainer } =
+    styles[variation];
+
   return (
     <button
-      className={`group relative w-full text-left bg-white py-16 sm:py-24 lg:py-32 ${
-        editable && "rounded border-2 border-transparent hover:border-indigo-500"
+      className={`group relative w-full bg-white py-16 text-left sm:py-24 lg:py-32 ${
+        editable &&
+        "rounded border-2 border-transparent hover:border-indigo-500"
       }`}
       onClick={() => handleClick()}
     >
-      <EditComponent id={id}/>
+      <EditComponent id={id} />
       <AddSectionButtons
-       id={id}
+        id={id}
         setSectionModal={setSectionModal}
         setTriggerSection={setTriggerSection}
       />
-      <div className="mx-auto grid max-w-7xl grid-cols-1 gap-10 px-6 lg:grid-cols-12 lg:gap-8 lg:px-8">
-        <div className="max-w-xl tracking-tight text-gray-900 flex flex-col gap-5  lg:col-span-7">
+      <div className={container}>
+        <div className="flex max-w-xl flex-col gap-5 tracking-tight text-gray-900  lg:col-span-7">
           <h2
-            className={`inline  text-3xl sm:text-4xl font-bold sm:block lg:inline xl:block ${
+            className={`inline  text-3xl font-bold sm:block sm:text-4xl lg:inline xl:block ${
               editable &&
               "rounded border-2 border-transparent hover:border-indigo-500"
             }`}
@@ -125,7 +176,7 @@ export default function NewsLetterSection(props: TProps) {
             }}
           >
             {isLoading ? ( // Conditional rendering based on loading state
-              <Skeleton className="h-6 w-96 mt-2" /> // Example skeleton size
+              <Skeleton className="mt-2 h-6 w-96" /> // Example skeleton size
             ) : (
               appState.aiContent?.newsLetter?.description ?? ""
             )}
