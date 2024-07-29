07e7b81e593c9938d21a8eda62e806dd9015ab91
Skysparko <shubhamrakhecha5@gmail.com>
2024-07-30 01:21:27 +0530

    restyle done

--- components/sections/pricing/index.tsx
+++ components/sections/pricing/index.tsx
@@ -1,13 +1,14 @@
-import { CheckIcon } from "@heroicons/react/20/solid";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
-import AddSectionButtons from "@/components/add-section/buttons";
-import { useAppDispatch, useAppSelector } from "@/lib/store/hooks";
-import CustomContent from "@/lib/content/custom";
-import { TFields, TSection, TSectionsType } from "@/types";
+import AddSectionButtons from '@/components/add-section/buttons';
+import CustomContent from '@/lib/content/custom';
+import { useAppDispatch, useAppSelector } from '@/lib/store/hooks';
+import { TFields, TSection } from '@/types';
+import { CheckIcon } from '@heroicons/react/20/solid';
+import { useEffect, useState } from 'react';
 
-import { Skeleton } from "@/components/ui/skeleton"; // Import the Skeleton component here
-import { updateAppState, appState as AS } from "@/lib/store/slices/site-slice";
-import EditComponent from "@/components/edit-component";
+import EditComponent from '@/components/edit-component';
+import { Skeleton } from '@/components/ui/skeleton'; // Import the Skeleton component here
+import { sectionsData as SD } from '@/lib/store/slices/section-slice';
+import { appState as AS, updateAppState } from '@/lib/store/slices/site-slice';
 
 type TProps = {
   editable?: boolean;
@@ -34,7 +35,7 @@ type TProps = {
 };
 
 function classNames(...classes: any[]) {
-  return classes.filter(Boolean).join(" ");
+  return classes.filter(Boolean).join(' ');
 }
 
 export default function PricingSection(props: TProps) {
@@ -54,18 +55,77 @@ export default function PricingSection(props: TProps) {
   const dispatch = useAppDispatch();
   const [isLoading, setLoading] = useState(true); // State to manage loading state
 
+  const sections = useAppSelector(SD);
+
+  // Find the section by ID and get the variation
+  const section = sections.find((section) => section.id === id);
+  const variation = section?.variation || 1;
+
+  const styles: {
+    [key: number]: {
+      container: string;
+      listContainer: string;
+      listCard: string;
+      contentContainer: string;
+    };
+  } = {
+    1: {
+      container: 'mx-auto max-w-7xl px-6 lg:px-8',
+      listContainer:
+        'isolate mx-auto mt-16 grid max-w-md grid-cols-1 gap-y-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3',
+      listCard:
+        'flex flex-col justify-between rounded-3xl bg-white p-8 ring-1 ring-gray-200 xl:p-10',
+      contentContainer: 'mx-auto max-w-4xl text-center',
+    },
+    2: {
+      container: 'mx-auto max-w-7xl px-6 lg:px-8',
+      listContainer:
+        'isolate mx-auto mt-16 grid max-w-md grid-cols-1 gap-y-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3 gap-10',
+      listCard:
+        'flex flex-col justify-between rounded-3xl bg-white p-8 ring-1 ring-gray-200 xl:p-10 rounded-xl',
+      contentContainer: 'mx-auto max-w-4xl text-left',
+    },
+    3: {
+      container: 'mx-auto max-w-7xl px-6 lg:px-8',
+      listContainer:
+        'isolate mx-auto mt-16 grid max-w-md grid-cols-1 gap-y-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3 gap-10',
+      listCard:
+        'flex flex-col justify-between rounded-3xl bg-white p-8 ring-1 ring-gray-200 xl:p-10 rounded-xl',
+      contentContainer: 'mx-auto max-w-4xl text-center',
+    },
+    4: {
+      container: 'mx-auto max-w-7xl px-6 lg:px-8',
+      listContainer:
+        'isolate mx-auto mt-16 grid max-w-md grid-cols-1 gap-y-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3',
+      listCard:
+        'flex flex-col justify-between rounded-3xl bg-white p-8 ring-1 ring-gray-200 xl:p-10',
+      contentContainer: 'mx-auto max-w-4xl text-left',
+    },
+    5: {
+      container: 'mx-auto max-w-7xl px-6 lg:px-8',
+      listContainer:
+        'isolate mx-auto mt-16 grid max-w-md grid-cols-1 gap-y-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3',
+      listCard:
+        'flex flex-col justify-between rounded-3xl bg-white p-8 ring-1 ring-gray-200 xl:p-10',
+      contentContainer: 'mx-auto max-w-4xl text-center',
+    },
+  };
+
+  const { container, listContainer, listCard, contentContainer } =
+    styles[variation];
+
   const handleClick = (field?: TFields) => {
     if (editable && setIsOpen && setSection) {
-      setSection("Pricing");
+      setSection('Pricing');
       setIsOpen(true);
 
-      setShowForm({ form: "", edit: "", show: false });
+      setShowForm({ form: '', edit: '', show: false });
       dispatch(
         updateAppState({
           ...appState,
           focusedField: field,
-          openedSlide: "Customize",
-        }),
+          openedSlide: 'Customize',
+        })
       );
     }
   };
@@ -75,12 +135,12 @@ export default function PricingSection(props: TProps) {
     CustomContent.getPricing({
       data: {
         businessName: appState.aiContent.banner.businessName,
-        businessType: appState.aiContent.businessType ?? "",
-        location: appState.aiContent.location ?? "",
+        businessType: appState.aiContent.businessType ?? '',
+        location: appState.aiContent.location ?? '',
       },
-      fieldName: "pricing",
+      fieldName: 'pricing',
       individual: false,
-      type: "list",
+      type: 'list',
     }).then(() => {
       setLoading(false); // Set loading to false when data fetch completes
     });
@@ -90,7 +150,7 @@ export default function PricingSection(props: TProps) {
     <button
       className={`group relative w-full bg-white py-24 sm:py-32 ${
         editable &&
-        "rounded border-2 border-transparent hover:border-indigo-500"
+        'rounded border-2 border-transparent hover:border-indigo-500'
       }`}
       onClick={() => handleClick()}
     >
@@ -100,44 +160,45 @@ export default function PricingSection(props: TProps) {
         setSectionModal={setSectionModal}
         setTriggerSection={setTriggerSection}
       />
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-4xl text-center">
+      <div className={container}>
+        <div className={contentContainer}>
           <h2 className="text-base font-semibold leading-7 text-indigo-600">
             Pricing
           </h2>
           <p
             className={`mx-auto mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl ${
               editable &&
-              "rounded border-2 border-transparent hover:border-indigo-500"
+              'rounded border-2 border-transparent hover:border-indigo-500'
             }`}
           >
             {isLoading ? (
               <Skeleton className="h-12 w-64" />
             ) : (
-              appState.aiContent?.pricing?.title ?? ""
+              appState.aiContent?.pricing?.title ?? ''
+            )}
+          </p>
+          <p
+            className={`mx-auto mt-6 max-w-2xl  text-lg leading-8 text-gray-600 ${
+              editable &&
+              'rounded border-2 border-transparent hover:border-indigo-500'
+            }`}
+          >
+            {isLoading ? (
+              <Skeleton className="h-6 w-96" />
+            ) : (
+              appState.aiContent?.pricing?.description ?? ''
             )}
           </p>
         </div>
-        <p
-          className={`mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600 ${
-            editable &&
-            "rounded border-2 border-transparent hover:border-indigo-500"
-          }`}
-        >
-          {isLoading ? (
-            <Skeleton className="h-6 w-96" />
-          ) : (
-            appState.aiContent?.pricing?.description ?? ""
-          )}
-        </p>
-        <div className="isolate mx-auto mt-16 grid max-w-md grid-cols-1 gap-y-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+
+        <div className={listContainer}>
           {(appState.aiContent?.pricing?.list ?? []).length === 0
             ? Array.from({ length: 3 }).map((_, idx) => (
                 <div
                   key={idx}
-                  className={`flex flex-col justify-between rounded-3xl bg-white p-8 ring-1 ring-gray-200 xl:p-10 ${
+                  className={`${listCard} ${
                     editable &&
-                    "rounded border-2 border-transparent hover:border-indigo-500"
+                    'rounded border-2 border-transparent hover:border-indigo-500'
                   }`}
                 >
                   <Skeleton className="mb-4 h-6 w-24" />
@@ -157,8 +218,8 @@ export default function PricingSection(props: TProps) {
                     href="#"
                     className={`mt-8 block rounded-md px-3 py-2 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${
                       editable
-                        ? "bg-indigo-600 text-white shadow-sm hover:bg-indigo-500"
-                        : "text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300"
+                        ? 'bg-indigo-600 text-white shadow-sm hover:bg-indigo-500'
+                        : 'text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300'
                     }`}
                   >
                     <Skeleton className="h-6 w-20" />
@@ -170,20 +231,20 @@ export default function PricingSection(props: TProps) {
                   <div
                     key={tier.id}
                     className={`${
-                      tier.mostPopular ? "lg:z-10 lg:rounded-b-none" : "lg:mt-8"
-                    } ${tierIdx === 0 ? "lg:rounded-r-none" : ""} ${
+                      tier.mostPopular ? 'lg:z-10 lg:rounded-b-none' : 'lg:mt-8'
+                    } ${tierIdx === 0 ? 'lg:rounded-r-none' : ''} ${
                       tierIdx === appState.aiContent?.pricing?.list.length - 1
-                        ? "lg:rounded-l-none"
-                        : ""
-                    } ${"flex flex-col justify-between rounded-3xl bg-white p-8 ring-1 ring-gray-200 xl:p-10"} ${
+                        ? 'lg:rounded-l-none'
+                        : ''
+                    } ${listCard} ${
                       editable &&
-                      "rounded border-2 border-transparent hover:border-indigo-500"
+                      'rounded border-2 border-transparent hover:border-indigo-500'
                     }`}
                     onClick={(e) => {
                       e.stopPropagation();
                       setShowForm({
                         edit: tier.id,
-                        form: "Price",
+                        form: 'Price',
                         show: true,
                       });
                     }}
@@ -194,9 +255,9 @@ export default function PricingSection(props: TProps) {
                           id={tier.id}
                           className={`${classNames(
                             tier.mostPopular
-                              ? "text-indigo-600"
-                              : "text-gray-900",
-                            "text-lg font-semibold leading-8",
+                              ? 'text-indigo-600'
+                              : 'text-gray-900',
+                            'text-lg font-semibold leading-8'
                           )}`}
                         >
                           {isLoading ? (
@@ -213,7 +274,7 @@ export default function PricingSection(props: TProps) {
                       </div>
                       <p
                         className={`mt-4 text-sm leading-6 text-gray-600 ${
-                          isLoading && "opacity-0"
+                          isLoading && 'opacity-0'
                         }`}
                       >
                         {isLoading ? (
@@ -242,7 +303,7 @@ export default function PricingSection(props: TProps) {
                           <li
                             key={feature}
                             className={`flex items-center gap-x-3 ${
-                              isLoading && "opacity-0"
+                              isLoading && 'opacity-0'
                             }`}
                           >
                             <CheckIcon
@@ -263,19 +324,19 @@ export default function PricingSection(props: TProps) {
                       aria-describedby={tier.id}
                       className={`${classNames(
                         tier.mostPopular
-                          ? "bg-indigo-600 text-white shadow-sm hover:bg-indigo-500"
-                          : "text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300",
-                        "mt-8 block rounded-md px-3 py-2 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
+                          ? 'bg-indigo-600 text-white shadow-sm hover:bg-indigo-500'
+                          : 'text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300',
+                        'mt-8 block rounded-md px-3 py-2 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
                       )}`}
                     >
                       {isLoading ? (
                         <Skeleton className="h-6 w-20" />
                       ) : (
-                        tier.button ?? "Buy Now"
+                        tier.button ?? 'Buy Now'
                       )}
                     </a>
                   </div>
-                ),
+                )
               )}
         </div>
       </div>
